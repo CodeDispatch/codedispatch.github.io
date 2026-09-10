db1648345822229d5bdb7fac0488dac9ecacd479
Intense <intensed01@gmail.com>
2026-09-10 23:33:26 +0200

    Add Image component with props and styling

---
+++ docs/.vitepress/components/Image.vue
@@ -0,0 +1,94 @@
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    default: ''
+  },
+  caption: {
+    type: String,
+    default: ''
+  },
+  source: {
+    type: String,
+    default: ''
+  },
+  sourceUrl: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <figure class="image-caption">
+    <img
+      :src="src"
+      :alt="alt"
+      class="image"
+    />
+
+    <figcaption v-if="caption || source">
+      <div v-if="caption" class="caption">
+        {{ caption }}
+      </div>
+
+      <div v-if="source" class="source">
+        Quelle:
+        <a
+          v-if="sourceUrl"
+          :href="sourceUrl"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {{ source }}
+        </a>
+        <span v-else>{{ source }}</span>
+      </div>
+    </figcaption>
+  </figure>
+</template>
+
+<style scoped>
+.image-caption {
+  margin: 2rem 0;
+  width: 100%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+  border-radius: 14px;
+}
+
+figcaption {
+  margin-top: 0.65rem;
+}
+
+.caption {
+  font-size: 0.85rem;
+  line-height: 1.45;
+  color: var(--vp-c-text-2);
+}
+
+.source {
+  margin-top: 0.2rem;
+  font-size: 0.72rem;
+  line-height: 1.4;
+  color: var(--vp-c-text-3);
+}
+
+.source a {
+  color: inherit;
+  text-decoration: underline;
+  text-underline-offset: 2px;
+}
+
+.source a:hover {
+  color: var(--vp-c-brand-1);
+}
+</style>
